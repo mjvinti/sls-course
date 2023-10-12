d6ed1d1851457db17b54be0140d29a332c1b3f2d
mjvinti <mjvinti@gmail.com>
2023-10-12 15:55:09 -0400

    successfully set up and sent test email

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+const AWS = require('aws-sdk');
+
+const ses = new AWS.SES({ region: 'us-east-1' });
+
 const sendMail = async (event, conext) => {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'email',
+    Destination: {
+      ToAddresses: ['email']
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello There, Obi Wan Kenobi!'
+        }
+      },
+      Subject: {
+        Data: 'Test Mail'
+      }
+    }
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = { handler: sendMail };
