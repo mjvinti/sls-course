3ca37e2d014a68f4dbed83584a293b219b473e74
mjvinti <mjvinti@gmail.com>
2023-10-16 10:07:47 -0400

    added quick error handling fix

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,3 +1,7 @@
+const createErrors = require('http-errors');
+const httpErrorHandler = require('@middy/http-error-handler');
+const middy = require('@middy/core');
+
 const { getAuctionById } = require('./getAuction');
 const { uploadPictureToS3 } = require('../lib/uploadPictureToS3');
 
@@ -11,10 +15,20 @@ const uploadAuctionPicture = async (event, context) => {
   const base64 = body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-  console.log(uploadToS3Result);
+  try {
+    const uploadToS3Result = await uploadPictureToS3(
+      auction.id + '.jpg',
+      buffer
+    );
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createErrors.InternalServerError(error);
+  }
 
   return { statusCode: 200, body: JSON.stringify({}) };
 };
 
-module.exports = { handler: uploadAuctionPicture };
+module.exports = {
+  handler: middy(uploadAuctionPicture).use(httpErrorHandler())
+};
