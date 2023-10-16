11386195c3f8ab924b4d004832531a95d564845c
mjvinti <mjvinti@gmail.com>
2023-10-16 09:59:12 -0400

    uploading picture to s3

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,19 @@
+const { getAuctionById } = require('./getAuction');
+const { uploadPictureToS3 } = require('../lib/uploadPictureToS3');
+
 const uploadAuctionPicture = async (event, context) => {
+  const {
+    body,
+    pathParameters: { id }
+  } = event;
+
+  const auction = await getAuctionById(id);
+  const base64 = body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+  console.log(uploadToS3Result);
+
   return { statusCode: 200, body: JSON.stringify({}) };
 };
 
---
+++ auction-service/src/lib/uploadPictureToS3.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+const uploadPictureToS3 = async (key, body) => {
+  const result = await s3
+    .upload({
+      Body: body,
+      Bucket: process.env.AUCTIONS_BUCKET_NAME,
+      ContentEncoding: 'base64',
+      ContentType: 'image/jpeg',
+      Key: key
+    })
+    .promise();
+  return result;
+};
+
+module.exports = { uploadPictureToS3 };
